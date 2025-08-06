dd1f4751b56934c6fc34e6977a0cd0ea72ad1758
teramako <teramako@gmail.com>
2025-08-06 14:41:59 +0000

    :green_heart: テスト実行の対象を絞れるようにする

--- test/test.ps1
+++ test/test.ps1
@@ -1,14 +1,24 @@
 <#
 .SYNOPSIS
 Test OreOreSec
+
+.PARAMETER FullName
+テストのフィルター
 #>
-param()
+param(
+    [Parameter()]
+    [string[]] $FullName
+)
 
 $ParnetDir = Join-Path -Path $PSScriptRoot -ChildPath .. -Resolve
 Import-Module (Join-Path -Path $ParnetDir -ChildPath OreOreSec.psd1)
 Import-Module -Name Pester
 
 $conf = New-PesterConfiguration
+if ($FullNames.Count -gt 0)
+{
+    $conf.Filter.FullName = $FullName
+}
 $conf.Run.Path = $PSScriptRoot
 # $conf.Debug.ReturnRawResultObject = $true
 # $conf.Debug.WriteDebugMessages = 
