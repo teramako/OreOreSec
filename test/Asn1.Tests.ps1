8dda1f9778f9bbe80eefd8f13b3c6a134b051937
teramako <teramako@gmail.com>
2025-08-06 15:47:24 +0000

    :green_heart: `Show-Asn1Tree` のテストを追加

--- test/Asn1.Tests.ps1
+++ test/Asn1.Tests.ps1
@@ -34,4 +34,38 @@ Describe 'Asn1' {
             $byteData | ConvertTo-Asn1 | Should -ExpectedType MT.Asn1.Asn1Data
         }
     }
+
+    Context 'Show-Asn1Tree' {
+        It 'From Asn1: <name>' -ForEach @(
+            @{ name = 'ecdsa.pkcs1.der'; dir = 'PrivateKey' }
+        ) {
+            $asn1Data = ConvertTo-Asn1 -Data ([IO.File]::ReadAllBytes((Join-Path -Path $privateKeyDir -ChildPath $name)))
+            Show-Asn1Tree -Asn1 $asn1Data
+            $asn1Data | Show-Asn1Tree | Out-String -Width 120 | Write-Host -ForegroundColor DarkGray
+        }
+
+        It 'From PEM: <name>' -ForEach @(
+            @{ name = 'ecdsa.pkcs1.pem'; dir = 'PrivateKey' }
+        ) {
+            $pemData = Read-PEM -Path (Join-Path -Path $privateKeyDir -ChildPath $name)
+            Show-Asn1Tree -PEM $pemData
+            $pemData | Show-Asn1Tree | Out-String -Width 120 | Write-Host -ForegroundColor DarkGray
+        }
+
+        It 'From Base64: <name>' -ForEach @(
+            @{ name = 'ecdsa.pkcs8.pem'; dir = 'PrivateKey' }
+        ) {
+            $pemData = Read-PEM -Path (Join-Path -Path $privateKeyDir -ChildPath $name)
+            Show-Asn1Tree -Base64 $pemData.Base64Data
+            $pemData.Base64Data | Show-Asn1Tree | Out-String -Width 120 | Write-Host -ForegroundColor DarkGray
+        }
+
+        It 'From Binary: <name>' -ForEach @(
+            @{ name = 'rsa.pkcs8.pem'; dir = 'PrivateKey' }
+        ) {
+            $pemData = Read-PEM -Path (Join-Path -Path $privateKeyDir -ChildPath $name)
+            Show-Asn1Tree -Data $pemData.GetRawData()
+            $pemData.GetRawData() | Show-Asn1Tree | Out-String -Width 120 | Write-Host -ForegroundColor DarkGray
+        }
+    }
 }
